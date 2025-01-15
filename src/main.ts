c381a9366bcca3d96d8c2b9e05906833d17e9566
Rishika Dubey <rishikadubey99@gmail.com>
2025-01-16 00:36:18 +0530

    Add comments

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+// Author: Rishika Dubey | Version: 1.0.0 | Date: 2025-01-14
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
